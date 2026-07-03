53b48f286bd10b1388a03fba1e18d759329b904f
imaank00 <imulesk17@gmail.com>
2026-07-03 14:10:29 -0700

    Create Blink_Two_LEDs_Separately.ino
    LED 1 is on for one second while LED 2 is off, then LED 1 is off for a second while LED 2 is on

---
+++ Blink_Two_LEDs_Separately/Blink_Two_LEDs_Separately.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(12,OUTPUT); // Initialize pin 12 as output
+  pinMode(8,OUTPUT); // Initialize pin 8 as output
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(12,HIGH); // LED 1 on
+  digitalWrite(8,LOW); // LED 2 off
+  delay(1000); // For one second
+
+  digitalWrite(12,LOW); // LED 1 off
+  digitalWrite(8,HIGH); // LED 2 on
+  delay(1000); // For one second
+}
