2bdbd7c388da1e09f82566c0d96733b16eda5116
imaank00 <imulesk17@gmail.com>
2026-07-03 14:14:42 -0700

    Update Blink_Two_LEDs_Separately.ino

--- Blink_Two_LEDs_Separately/Blink_Two_LEDs_Separately.ino
+++ Blink_Two_LEDs_Separately/Blink_Two_LEDs_Separately.ino
@@ -1,3 +1,5 @@
+// Breadboard and Arduino connections are same as for Blink_Two_LEDs_Simultaneously
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(12,OUTPUT); // Initialize pin 12 as output
